c7e653384136ec6105520453636965f58f740abe
BhavanaVidala <bhavana82vidala@gmail.com>
2021-03-05 12:36:55 +0530

    FBVCommit

--- DBProject1/DBApp1/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from DBApp1.models import Student
-
-# Register your models here.
-class StudentAdmin(admin.ModelAdmin):
-    l=['number','name','marks']
-    
-admin.site.register(Student,StudentAdmin)
-    
--- DBProject1/DBApp1/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from DBApp1.models import Student
-
-# Create your views here.
-def view1(request):
-    e=Student.objects.all()
-    print(type(e))
-    d={'emp':e}
-    return render(request,'DBApp1/1.html',d)
